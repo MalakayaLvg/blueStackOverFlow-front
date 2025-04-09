44ca85722c399b397053eab7e35d6f5b01b1fb83
Oslynx <malakayalvg@gmail.com>
2025-04-10 00:09:25 +0200

    post edited + answer edited

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -17,6 +17,8 @@ const answerBody = ref("")
 const isEditing = ref(false)
 const editTitle = ref("")
 const editBody = ref("")
+const isEditingAnswer = ref(null)
+const editAnswerBody = ref("")
 
 const fetchPost = () => {
   fetch(`http://localhost:8000/api/questions/${id.value}/`,{
@@ -192,6 +194,40 @@ const downVoteAnswer = (answerId) => {
       .catch(error => console.log(error))
 }
 
+const startEditingAnswer = (answer) => {
+  isEditingAnswer.value = answer.id
+  editAnswerBody.value = answer.body
+}
+
+
+const cancelEditingAnswer = () => {
+  isEditingAnswer.value = null
+  editAnswerBody.value = ""
+}
+
+
+const saveEditAnswer = (answerId) => {
+  const updatedAnswer = {
+    question: id.value,
+    body: editAnswerBody.value
+  }
+
+  fetch(`http://localhost:8000/api/answers/${answerId}/`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updatedAnswer)
+  })
+      .then(res => res.json())
+      .then(data => {
+        isEditingAnswer.value = null
+        fetchPost()
+      })
+      .catch(error => console.log(error))
+}
+
 onMounted(() => {
   fetchPost()
 })
@@ -267,27 +303,49 @@ onMounted(() => {
         <ul class="list-unstyled" v-for="answer in question.answers" :key="answer.id">
           <li class="mb-4 p-3 border-bottom">
             <h4>{{ answer.author }}</h4>
-            <p>{{ answer.body }}</p>
-            <div class="d-flex align-items-center">
-              <button v-on:click="upVoteAnswer(answer.id)"
-                      class="btn btn-outline-success me-1">
-                <i class="bi bi-arrow-up-circle"></i>
-              </button>
-              <span class="me-3">{{ answer.upvotes || 0 }}</span>
 
-              <button v-on:click="downVoteAnswer(answer.id)"
-                      class="btn btn-outline-danger me-1">
-                <i class="bi bi-arrow-down-circle"></i>
-              </button>
-              <span class="me-3">{{ answer.downvotes || 0 }}</span>
+            <!-- Affichage normal du commentaire -->
+            <div v-if="isEditingAnswer !== answer.id">
+              <p>{{ answer.body }}</p>
+              <div class="d-flex align-items-center">
+                <button v-on:click="upVoteAnswer(answer.id)"
+                        class="btn btn-outline-success me-1">
+                  <i class="bi bi-arrow-up-circle"></i>
+                </button>
+                <span class="me-3">{{ answer.upvotes || 0 }}</span>
 
-              <button v-on:click="deleteAnswer(answer.id)"
-                      class="btn btn-outline-danger me-2">
-                <i class="bi bi-trash"></i>
-              </button>
-              <button class="btn btn-outline-warning me-2">
-                <i class="bi bi-pen"></i>
-              </button>
+                <button v-on:click="downVoteAnswer(answer.id)"
+                        class="btn btn-outline-danger me-1">
+                  <i class="bi bi-arrow-down-circle"></i>
+                </button>
+                <span class="me-3">{{ answer.downvotes || 0 }}</span>
+
+                <button v-on:click="deleteAnswer(answer.id)"
+                        class="btn btn-outline-danger me-2">
+                  <i class="bi bi-trash"></i>
+                </button>
+                <button v-on:click="startEditingAnswer(answer)"
+                        class="btn btn-outline-warning me-2">
+                  <i class="bi bi-pen"></i>
+                </button>
+              </div>
+            </div>
+
+            <!-- Interface d'édition du commentaire -->
+            <div v-else class="mb-3">
+              <div class="mb-3">
+                <label for="edit-answer-body" class="form-label">Contenu</label>
+                <textarea
+                    class="form-control"
+                    id="edit-answer-body"
+                    rows="3"
+                    v-model="editAnswerBody"
+                ></textarea>
+              </div>
+              <div class="d-flex">
+                <button @click="saveEditAnswer(answer.id)" class="btn btn-primary me-2">Enregistrer</button>
+                <button @click="cancelEditingAnswer" class="btn btn-secondary">Annuler</button>
+              </div>
             </div>
           </li>
         </ul>
